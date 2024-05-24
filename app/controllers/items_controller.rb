40d4a70e19ef191b672037a8e0d4bf11ccca391b
ryochin33 <1210.ryo.i@gmail.com>
2024-05-24 18:09:03 +0900

    商品管理機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,12 +14,14 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+
       render :new
     end
   end
 
 
   private
+  
   def item_params
   params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_burden_id, :shipping_region_id, :shipping_time_id, :price, :image).merge(user_id: current_user.id)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :items
-  # has_many :orders
+   has_many :items
+   has_many :orders
 
   validates :nickname, presence: true
   validates :last_name, presence: true,format: { with: /\A[^\x01-\x7E]+\z/, message: "は全角文字を使用してください" }
